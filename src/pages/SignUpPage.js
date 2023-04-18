3c40d25b79d218d56b1f539064cf4c5cb085c893
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-04-18 00:17:26 -0300

    Criação dos estados da pagina de login

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
 
 export default function SignUpPage() {
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [confirmacaoSenha, setConfirmacaoSenha] = useState("")
+
+  console.log(nome + email + senha + confirmacaoSenha)
+
   return (
     <SingUpContainer>
       <form>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input placeholder="Nome" type="text" value={nome} onChange={e => setNome(e.target.value)} />
+        <input placeholder="E-mail" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input placeholder="Senha" type="password" autocomplete="new-password" value={senha} onChange={e => setSenha(e.target.value)} />
+        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" value={confirmacaoSenha} onChange={e => setConfirmacaoSenha(e.target.value)} />
         <button>Cadastrar</button>
       </form>
 
